7bd26208c1ce5622baa449737f45917f175cb0b5
Andre <andreksp@gmail.com>
2022-02-03 18:57:26 -0300

    add buildspec for pipeline

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const HOST = "0.0.0.0";
 
 const app = express();
 app.get("/", (req, res) => {
-    res.send(`Hello World Update 1.2 - ${new Date().toISOString()}`);
+    res.send(`Hello World Update 1.3 - ${new Date().toISOString()}`);
 });
 
 app.listen(PORT, HOST);
